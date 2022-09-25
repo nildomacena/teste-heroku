57411350cddb46ce9fb199217651198383f9fa74
Ednildo Macena <nildo.777@gmail.com>
2022-09-25 11:32:27 -0300

    port acima do nestfactory

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const port = parseInt(process.env.PORT) || 3000;
   console.log('port: ', port, typeof port)
+  const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }
 bootstrap();
