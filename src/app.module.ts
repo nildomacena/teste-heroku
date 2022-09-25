848a0c4fe9f70bd53d4983d4da0c7d52934f38e7
Ednildo Macena <nildo.777@gmail.com>
2022-09-25 08:49:35 -0300

    tentando conectar a aws

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'techcasp.cw5rwj0xy7gj.sa-east-1.rds.amazonaws.com',
+    port: 5432,
+    username: 'techcasp',
+    password: '21041898',
+    database: 'bd_techcasp',
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
