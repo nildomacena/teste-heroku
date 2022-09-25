35b1d7cf0a1866affce2397454cd55d6878cbf50
Ednildo Macena <nildo.777@gmail.com>
2022-09-25 11:25:48 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const port = parseInt(process.env.PORT) || 3000;
+  console.log('port: ', port, typeof port)
+  await app.listen(port);
 }
 bootstrap();
